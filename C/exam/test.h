ad7cde94f79778cc0045913b5bc0f230fed8d6ec
Harshit Harsh <harshitharsh659@gmail.com>
2026-03-26 00:50:23 +0530

    Delete C directory

--- C/exam/test.h
+++
@@ -1,2 +0,0 @@
-#define rec_area(a, b) (a*b)
-#define peri(r) (2*3.14*r)
--- C/test/size.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-printf("%d", sizeof(int));
-printf("%d", sizeof(char));
-printf("%d", sizeof(float));
-}
